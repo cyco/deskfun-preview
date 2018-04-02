b975ecd55db4fdf7031e21d11345a838bbb0bffc
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-04-02 16:05:06 +0200

    Add type to access color palette.

---
+++ src/palette.rs
@@ -0,0 +1,27 @@
+use std::io;
+use std::ops::Index;
+
+pub struct Palette {
+    data: [u8; 0x100],
+}
+
+pub enum Color {
+    Transparent,
+    RGB(u8, u8, u8),
+}
+
+impl Palette {
+    fn new(input: &mut io::Read) -> io::Result<Palette> {
+        let mut buffer = [0 as u8; 0x100];
+        input.read_exact(&mut buffer)?;
+
+        Ok(Palette { data: buffer })
+    }
+
+    fn at(&self, index: u8) -> Color {
+        match index {
+            0 => Color::Transparent,
+            i => Color::RGB(4 * i + 2, 4 * i + 1, 4 * i + 0),
+        }
+    }
+}
