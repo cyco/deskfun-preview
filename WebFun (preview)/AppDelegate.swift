cf57722c3d481a854df816dfbf19f2f2d4224b40
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-04-04 21:56:23 +0200

    Add stub application to make QuickLook recognize the document type.

---
+++ WebFun (preview)/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  WebFun (preview)
+//
+//  Created by Christoph Leimbrock on 03.04.18.
+//  Copyright © 2018 Christoph Leimbrock. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
+
