fd892b9bd6c963d46b76e4b5d3821c7e806d1d78
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-04-04 21:55:55 +0200

    Use renderer for thumbnail generation.

--- src/GeneratePreviewForURL.c
+++ src/GeneratePreviewForURL.c
@@ -2,6 +2,10 @@
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
 
+#define BUNDLE_IDENTIFIER "de.ccl.webfun.qlgenerator"
+
+extern OSStatus generate_thumbnail(const char * bundlePath, const char * gamePath, size_t* length, uint8 **buffer);
+
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 
@@ -13,15 +17,27 @@ void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
-    printf("GeneratePreviewForURL\n");
-    
     CFStringRef path = CFURLCopyFileSystemPath(url, kCFURLPOSIXPathStyle);
-    printf("\t url: %s\n", CFStringGetCStringPtr(path, kCFStringEncodingUTF8));
+    CFBundleRef bundle = CFBundleGetBundleWithIdentifier(CFSTR(BUNDLE_IDENTIFIER));
+    CFURLRef bundleURL = CFBundleCopyBundleURL(bundle);
+    CFStringRef bundlePath = CFURLCopyFileSystemPath(bundleURL, kCFURLPOSIXPathStyle);
+    
+    const char * bundle_path = CFStringGetCStringPtr(bundlePath, kCFStringEncodingUTF8);
+    const char * cpath = CFStringGetCStringPtr(path, kCFStringEncodingUTF8);
+    
+    size_t length = 0;
+    uint8 *buffer = NULL;
+    OSStatus result = generate_thumbnail(bundle_path, cpath, &length, &buffer);
+    
+    CFDataRef imageData = CFDataCreateWithBytesNoCopy(NULL, buffer, length, NULL);
+    QLPreviewRequestSetDataRepresentation(preview, imageData, kUTTypeImage, NULL);
+    CFRelease(imageData);
+    
+    CFRelease(bundlePath);
+    CFRelease(bundleURL);
     CFRelease(path);
     
-    
-    
-    return noErr;
+    return result;
 }
 
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview)
