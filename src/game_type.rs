dfbd7557a37ce9d8a27e50344ed45231e42581c8
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-04-07 15:02:50 +0200

    Add trait to read game type from save game buffer.

--- src/game_type.rs
+++ src/game_type.rs
@@ -5,7 +5,7 @@ use std::io::Read;
 use std::path;
 use std::path::Path;
 
-#[derive(PartialEq, Copy, Clone)]
+#[derive(PartialEq, Copy, Clone, Debug)]
 pub enum GameType {
     Yoda,
     Indy,
@@ -47,3 +47,21 @@ pub fn build_palette_path(base_path: &path::Path, game_type: &GameType) -> path:
         GameType::Indy => base_path.join(path::Path::new("Contents/Resources/indy.pal")),
     }
 }
+
+pub trait ReadSaveGameTypeExt: io::Read {
+    fn read_save_game_type(&mut self) -> io::Result<GameType> {
+        let mut buffer = vec!(0; 9);
+        self.read_exact(&mut buffer)?;
+
+        match String::from_utf8(buffer).expect("").as_str() {
+            "YODASAV44" => Ok(GameType::Yoda),
+            "INDYSAV44" => Ok(GameType::Indy),
+            _ => Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Valid file header was not found.",
+            )),
+        }
+    }
+}
+
+impl<R: io::Read + ?Sized> ReadSaveGameTypeExt for R {}
